fb2b1825fad45fe382ee85fa7eda475a4949752b
josue <joshlezher@gmail.com>
2023-04-12 21:55:03 -0600

    se agrega el proyecto

---
+++ src/lab05/nodoarbol.java
@@ -0,0 +1,26 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab05;
+
+/**
+ *
+ * @author josh
+ */
+public class nodoarbol {
+    int dato;
+    String nombre;
+    nodoarbol hijoIzquierdo, hijoDerecho;
+    public nodoarbol(int d, String nom){
+        this.dato=d;
+        this.nombre=nom;
+        this.hijoDerecho=null;
+        this.hijoIzquierdo=null;
+    }
+    public String toString(){
+        return nombre + "su datos es" + dato;
+    }
+    
+}
